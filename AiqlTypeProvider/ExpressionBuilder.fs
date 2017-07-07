4723e939dd3d0e66fd512adc21bcf997f1c8612d
kcheick <kcheick@gmail.com>
2017-07-07 11:56:13 -0700

    initial commit

---
+++ AiqlTypeProvider/ExpressionBuilder.fs
@@ -0,0 +1,42 @@
+﻿module ExpressionBuilder
+open System
+open System.Reflection
+
+type IAiqlQuery<'T> = 
+    abstract Expression : Quotations.Expr<'T>
+
+type SingleResultQuery<'T> = 
+    inherit IAiqlQuery<'T>
+
+let getTable (tableName:String) = new NotImplementedException () |> raise
+let where  ([<ReflectedDefinition>] predicate ) query = new NotImplementedException () |> raise
+let count (query:Quotations.Expr<'T[]>) : Quotations.Expr<'T> = new NotImplementedException () |> raise
+
+let rec toAiql expr = 
+    match expr with
+    | Quotations.Patterns.Value (v,t) -> v.ToString()
+    | Quotations.Patterns.PropertyGet (obj, propertyInfo, _) -> propertyInfo.Name
+    | Quotations.Patterns.Var vr -> vr.Name
+    | Quotations.DerivedPatterns.SpecificCall <@ getTable @> (_,_, [Quotations.Patterns.Value (v,t)]) ->
+        v.ToString()
+    | Quotations.DerivedPatterns.SpecificCall <@ where @> (_,_ ,whereArgs) -> 
+        match whereArgs with
+        | [Quotations.Patterns.Lambda ( arg,body);queryExpression ] ->
+            match body with
+            | Quotations.DerivedPatterns.SpecificCall <@ (=) @> (_,_,[left;right]) ->
+                sprintf "%s | where (%s == %s)" (toAiql queryExpression) (toAiql left) (toAiql right)
+            | _ -> "Nothing"
+        | _ -> "Nothing"
+    | _ -> "Nothing"
+
+[<NoComparison>]
+type QueryExpresion<'T> = {
+    Expression: Quotations.Expr<'T>
+}
+
+type QueryExpresion = 
+    static member Where ([<ReflectedDefinition>] predicate:Quotations.Expr<'a -> bool> )  =
+                fun (this:QueryExpresion<'a[]>) -> { this with Expression =  <@ where %predicate %this.Expression @>}
+        
+    
+    
---
+++ AiqlTypeProvider/TypeProvider.fs
@@ -0,0 +1,59 @@
+﻿namespace AiqlTypeProvider
+
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Core.CompilerServices
+open System
+open System.Reflection
+open System.Net.Http
+open Newtonsoft.Json
+
+type ColumnDefinition = {
+    ColumnName: string
+    DataType: string
+    ColumnType: string
+}
+
+type TableData = {
+    TableName: string
+    Columns: ColumnDefinition[]
+    Rows: string[][]
+}
+
+type TableResult = {
+   Tables : TableData[] 
+}
+
+[<TypeProvider>]
+type AiqlTypeProvider (config : TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces ()
+
+    let ns = "AiqlTypeProvider"
+    let asm = Assembly.GetExecutingAssembly()
+
+    let createTypes () =
+        use client = new HttpClient()
+        client.DefaultRequestHeaders.Add("x-api-key","DEMO_KEY")
+        let result = 
+            client.GetAsync("https://api.applicationinsights.io/beta/apps/DEMO_APP/query/schema").Result.Content.ReadAsStringAsync().Result
+
+        let tableData =
+            JsonConvert.DeserializeObject<TableResult>(result)
+    
+        tableData.Tables
+        |> Seq.collect(fun x -> x.Rows)
+        |> Seq.map(fun x -> x.[0], x.[1], Type.GetType( x.[2]))
+        |> Seq.groupBy (fun (tableName, _, _) -> tableName )
+        |> Seq.map(fun (key, grp) -> 
+                            let myType = ProvidedTypeDefinition(key,  Some typeof<obj>)
+                            grp
+                            |> Seq.iter (fun (_, columnName, columnType) -> myType.AddMember <| ProvidedProperty(columnName, columnType, IsStatic = true))
+                            myType
+                            )
+        |> Seq.toList
+                                    
+
+    do
+        this.AddNamespace(ns, createTypes())
+
+[<assembly:TypeProviderAssembly>]
+do ()
