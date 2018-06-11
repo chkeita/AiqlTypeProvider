e0ee7b83117dcdbfe15a47a7f3cdb08d17fded04
Cheick Keita <kcheick@gmail.com>
2018-06-11 07:53:46 -0700

    adding test project

---
+++ testing/ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open AzureQueryTypeProvider
+
+type appinsight = AzureQueryTypeProvider.ApplicationInsights<"demo", "demo">
+
+[<EntryPoint>]
+let main argv = 
+    let x = appinsight.Context.QueryData(appinsight.requests |> ExpressionBuilder.Expression.take 10) |> Async.RunSynchronously
+    let head = Seq.head x
+
+    let t = appinsight.requests.GetType()
+    printfn "%O" "fsdsd"
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
